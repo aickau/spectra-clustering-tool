a9eda11989516dbdae9554dcd2144b06828d1cb0
Administrator <Administrator@968c2a35-807f-3149-efd7-b5b61160118a>
2009-05-26 21:53:59 +0000

    -removed parallelization which reduced quality of the results -this should be the final version!

--- branches/diplomathesis_arjen/trunk/src/analyze/SOFMNetwork.h
+++ branches/diplomathesis_arjen/trunk/src/analyze/SOFMNetwork.h
@@ -152,12 +152,6 @@ protected:
 		size_t _numBestMatchElements, 
 		bool _bOnFrameOnly = false );
 
-	// search for best matching spectrum/neuron in the network using only a local window ( s_searchRadius )
-	void searchBestMatchLocal( const std::vector<size_t> &_spectraIndexList, 
-		size_t _spectraIndexListOffset, 
-		BestMatch *_pBestMatchBatch, 
-		size_t _numBestMatchElements );
-
 	// adapt network for a given neuron/spectrum
 	// _spectrum source spectrum to adapt
 	// _bestMatchIndex index into network where it best matches
