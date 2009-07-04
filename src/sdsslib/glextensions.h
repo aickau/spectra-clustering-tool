721f406800e86788bf6383818ca88df13754e3cb
Administrator <Administrator@968c2a35-807f-3149-efd7-b5b61160118a>
2009-07-04 22:40:27 +0000

    -added fbo ext

--- src/sdsslib/glextensions.h
+++ src/sdsslib/glextensions.h
@@ -1,6 +1,27 @@
+//! \verbatim
+//! ###########################################################################
+//! # SDSS sorting prototype - Stage I
+//! #
+//! # Copyright (c) 2009 Aick in der Au
+//! # All rights reserved.
+//! ###########################################################################
+//!
+//!      created by : Aick in der Au <aick.inderau@gmail.com>
+//!      created on : 7/4/2009
+//! code taken from : Shiva of Kolor (but not sure)
+//! additional docs : none
+//!  responsibility : 1. Aick in der Au
+//!                   2. 
+//! \endverbatim
+//!
+//! \file  glextensions.h
+//! \brief 
+
+
 #ifndef H_GLEXTENSIONS
 #define H_GLEXTENSIONS
 
+#include <Windows.h>
 #include <gl/gl.h>
 #include "../glext.h"
 #include "../wglext.h"
@@ -17,26 +38,25 @@ extern PFNGLENABLEVERTEXATTRIBARRAYARBPROC	glEnableVertexAttribArrayARB ;
 extern PFNGLDISABLEVERTEXATTRIBARRAYARBPROC glDisableVertexAttribArrayARB ;
 
 
-
 extern PFNGLCREATESHADEROBJECTARBPROC		glCreateShaderObjectARB		;
 extern PFNGLISPROGRAMARBPROC				glIsProgramARB				;
-extern PFNGLGETPROGRAMIVARBPROC			glGetProgramivARB			;
-extern PFNGLSHADERSOURCEARBPROC			glShaderSourceARB			;
-extern PFNGLDELETEOBJECTARBPROC			glDeleteObjectARB			;
+extern PFNGLGETPROGRAMIVARBPROC				glGetProgramivARB			;
+extern PFNGLSHADERSOURCEARBPROC				glShaderSourceARB			;
+extern PFNGLDELETEOBJECTARBPROC				glDeleteObjectARB			;
 extern PFNGLCOMPILESHADERARBPROC			glCompileShaderARB			;
 extern PFNGLCREATEPROGRAMOBJECTARBPROC		glCreateProgramObjectARB	;
-extern PFNGLATTACHOBJECTARBPROC			glAttachObjectARB			;
-extern PFNGLDETACHOBJECTARBPROC			glDetachObjectARB			;
+extern PFNGLATTACHOBJECTARBPROC				glAttachObjectARB			;
+extern PFNGLDETACHOBJECTARBPROC				glDetachObjectARB			;
 extern PFNGLLINKPROGRAMARBPROC				glLinkProgramARB			;
-extern PFNGLUSEPROGRAMOBJECTARBPROC		glUseProgramObjectARB		;
-extern PFNGLGETOBJECTPARAMETERIVARBPROC	glGetObjectParameterivARB	;
+extern PFNGLUSEPROGRAMOBJECTARBPROC			glUseProgramObjectARB		;
+extern PFNGLGETOBJECTPARAMETERIVARBPROC		glGetObjectParameterivARB	;
 extern PFNGLGETINFOLOGARBPROC				glGetInfoLogARB				;
 extern PFNGLUNIFORM1IARBPROC				glUniform1iARB				;
 extern PFNGLUNIFORM3FVARBPROC				glUniform3fvARB				;
 extern PFNGLUNIFORM4FVARBPROC				glUniform4fvARB				;
 extern PFNGLGETUNIFORMLOCATIONARBPROC		glGetUniformLocationARB		;
 extern PFNGLUNIFORM1FARBPROC				glUniform1fARB				;
-//PFNGLUNIFORMMATRIX4FVARBPROC		glUniformMatrix4fvARB		;
+//PFNGLUNIFORMMATRIX4FVARBPROC			glUniformMatrix4fvARB		;
 
 
 extern PFNGLGETATTRIBLOCATIONARBPROC		glGetAttribLocationARB		;
@@ -45,40 +65,46 @@ extern PFNGLGETATTRIBLOCATIONARBPROC		glGetAttribLocationARB		;
 extern PFNGLCLIENTACTIVETEXTUREARBPROC		glClientActiveTextureARB	;
 extern PFNGLACTIVETEXTUREARBPROC			glActiveTextureARB			;
 extern PFNGLMULTITEXCOORD2FARBPROC			glMultiTexCoord2fARB		;
-extern PFNGLMULTITEXCOORD2FVARBPROC		glMultiTexCoord2fvARB		;
+extern PFNGLMULTITEXCOORD2FVARBPROC			glMultiTexCoord2fvARB		;
+
+// FBO Ext
+extern PFNGLGENFRAMEBUFFERSEXTPROC			glGenFramebuffersEXT;
+extern PFNGLDELETEFRAMEBUFFERSEXTPROC		glDeleteFramebuffersEXT;
+extern PFNGLBINDFRAMEBUFFEREXTPROC			glBindFramebufferEXT;
+extern PFNGLGENRENDERBUFFERSEXTPROC			glGenRenderbuffersEXT;
+extern PFNGLDELETERENDERBUFFERSEXTPROC		glDeleteRenderbuffersEXT;
+extern PFNGLBINDRENDERBUFFEREXTPROC			glBindRenderbufferEXT;
+extern PFNGLRENDERBUFFERSTORAGEEXTPROC		glRenderbufferStorageEXT;
+extern PFNGLFRAMEBUFFERRENDERBUFFEREXTPROC	glFramebufferRenderbufferEXT;
+extern PFNGLFRAMEBUFFERTEXTURE2DEXTPROC		glFramebufferTexture2DEXT;
+extern PFNGLCHECKFRAMEBUFFERSTATUSEXTPROC	glCheckFramebufferStatusEXT;
 
 // GL_ARB_vertex_buffer_object
-extern PFNGLBINDBUFFERARBPROC				glBindBufferARB				;
-extern PFNGLDELETEBUFFERSARBPROC			glDeleteBuffersARB			;
-extern PFNGLGENBUFFERSARBPROC				glGenBuffersARB				;
-extern PFNGLBUFFERDATAARBPROC				glBufferDataARB				;
-extern PFNGLMAPBUFFERARBPROC				glMapBufferARB				;
-extern PFNGLUNMAPBUFFERARBPROC				glUnmapBufferARB			;
-// WGL_EXT_swap_control_supported
-extern PFNWGLSWAPINTERVALEXTPROC			wglSwapIntervalEXT			;
+extern PFNGLBINDBUFFERARBPROC				glBindBufferARB;
+extern PFNGLDELETEBUFFERSARBPROC			glDeleteBuffersARB;
+extern PFNGLGENBUFFERSARBPROC				glGenBuffersARB;
+extern PFNGLBUFFERDATAARBPROC				glBufferDataARB;
+extern PFNGLMAPBUFFERARBPROC				glMapBufferARB;
+extern PFNGLUNMAPBUFFERARBPROC				glUnmapBufferARB;
 
+// WGL_EXT_swap_control_supported
+extern PFNWGLSWAPINTERVALEXTPROC			wglSwapIntervalEXT;
 
 // WGL_ARB_pixel_format
-extern PFNWGLGETPIXELFORMATATTRIBIVARBPROC	wglGetPixelFormatAttribivARB ;
-extern PFNWGLGETPIXELFORMATATTRIBFVARBPROC	wglGetPixelFormatAttribfvARB ;
-extern PFNWGLCHOOSEPIXELFORMATARBPROC		wglChoosePixelFormatARB		;
-
-// WGL_ARB_pbuffer
-extern PFNWGLCREATEPBUFFERARBPROC			wglCreatePbufferARB			; 
-extern PFNWGLGETPBUFFERDCARBPROC			wglGetPbufferDCARB			;
-extern PFNWGLRELEASEPBUFFERDCARBPROC		wglReleasePbufferDCARB		;
-extern PFNWGLDESTROYPBUFFERARBPROC			wglDestroyPbufferARB		;
-extern PFNWGLQUERYPBUFFERARBPROC			wglQueryPbufferARB			; 
+extern PFNWGLGETPIXELFORMATATTRIBIVARBPROC	wglGetPixelFormatAttribivARB;
+extern PFNWGLGETPIXELFORMATATTRIBFVARBPROC	wglGetPixelFormatAttribfvARB;
+extern PFNWGLCHOOSEPIXELFORMATARBPROC		wglChoosePixelFormatARB;
 
 // WGL_ARB_render_texture
-extern PFNWGLBINDTEXIMAGEARBPROC			wglBindTexImageARB			; 
-extern PFNWGLRELEASETEXIMAGEARBPROC		wglReleaseTexImageARB		; 
+extern PFNWGLBINDTEXIMAGEARBPROC			wglBindTexImageARB; 
+extern PFNWGLRELEASETEXIMAGEARBPROC			wglReleaseTexImageARB; 
+
 // WGL_ARB_extensions_string
-extern PFNWGLGETEXTENSIONSSTRINGARBPROC	wglGetExtensionsStringARB	;
+extern PFNWGLGETEXTENSIONSSTRINGARBPROC		wglGetExtensionsStringARB;
 
+namespace GLExtensions
+{
+	bool loadGLExtensions( bool _silent = true );
+}
 
-
-bool LoadWGLExtensions ( bool silent );
-bool LoadGLExtensions ( bool silent );
-
-#endif
+#endif // H_GLEXTENSIONS
