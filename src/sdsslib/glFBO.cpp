c5042cc431884db8606f7e9994c59a4f354e7568
Administrator <Administrator@968c2a35-807f-3149-efd7-b5b61160118a>
2009-07-05 20:46:35 +0000

    -added FBO impl

---
+++ src/sdsslib/glFBO.cpp
@@ -0,0 +1,151 @@
+//! \verbatim
+//! ###########################################################################
+//! # SDSS sorting prototype - Stage I
+//! #
+//! # Copyright (c) 2009 Aick in der Au
+//! # All rights reserved.
+//! ###########################################################################
+//!
+//!      created by : Aick in der Au <aick.inderau@gmail.com>
+//!      created on : 7/4/2009
+//! additional docs : none
+//!  responsibility : 1. Aick in der Au
+//!                   2. 
+//! \endverbatim
+//!
+//! \file  glFBO.cpp
+//! \brief 
+
+#include "sdsslib/glFBO.h"
+
+#include "sdsslib/glextensions.h"
+
+#include <assert.h>
+
+GLFBO::GLFBO( unsigned int _widht, unsigned int _height )
+:m_id(0)
+,m_oldId(0)
+,m_width(_widht)
+,m_height(_height)
+,m_bIsBound(false)
+{
+	glGenFramebuffersEXT(1, &m_id);
+	for (size_t i=0;i<RB_COUNT;i++)
+	{
+		m_renderBufferIDs[i] = 0;
+	}
+}
+
+GLFBO::~GLFBO()
+{
+	destroy();
+}
+
+void GLFBO::destroy()
+{
+	if ( m_bIsBound )
+	{
+		unbind();
+	}
+
+	glDeleteFramebuffersEXT(1, &m_id);
+
+	for (size_t i=0;i<RB_COUNT;i++)
+	{
+		if ( m_renderBufferIDs[i] != 0 )
+		{
+			glDeleteRenderbuffersEXT(1, &m_renderBufferIDs[i] );
+		}
+	}
+
+}
+
+
+unsigned int GLFBO::getID() const
+{
+	return m_id;
+}
+
+bool GLFBO::isComplete() const
+{
+	GLenum fboStatus = glCheckFramebufferStatusEXT( GL_FRAMEBUFFER_EXT );
+
+	return ( fboStatus == GL_FRAMEBUFFER_COMPLETE_EXT);
+}
+
+
+void GLFBO::bind()
+{
+	if ( isComplete() && !m_bIsBound )
+	{
+		GLint p;
+		glGetIntegerv( GL_FRAMEBUFFER_BINDING_EXT, &p );
+		m_oldId = p;
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, m_id);
+
+		glPushAttrib(GL_VIEWPORT_BIT);
+		glViewport(0,0,m_width, m_height);
+
+		m_bIsBound = true;
+	}
+}
+
+
+void GLFBO::unbind()
+{
+	if ( m_bIsBound )
+	{
+		glPopAttrib();
+		glBindFramebufferEXT(GL_FRAMEBUFFER_EXT, m_oldId);
+	}
+	else
+	{
+		assert(0);
+	}
+}
+
+GLenum GLFBO::getComponentFromType( RenderBufferType _type )
+{
+	if ( _type == RB_DEPTH )
+		return GL_DEPTH_COMPONENT;
+	if ( _type == RB_STENCIL )
+		return GL_STENCIL_INDEX8_EXT;
+	if ( _type == RB_COLOR0 )
+		return GL_RGBA;
+	if ( _type == RB_COLOR0 )
+		return GL_RGBA;
+
+	return GL_RGBA;
+}
+
+GLenum GLFBO::getAttachmentFromType( RenderBufferType _type )
+{
+	if ( _type == RB_COLOR0 )
+		return GL_COLOR_ATTACHMENT0_EXT;
+	if ( _type == RB_COLOR1 )
+		return GL_COLOR_ATTACHMENT1_EXT;
+	if ( _type == RB_DEPTH )
+		return GL_DEPTH_ATTACHMENT_EXT;
+	if ( _type == RB_STENCIL )
+		return GL_STENCIL_ATTACHMENT_EXT;
+
+	return GL_COLOR_ATTACHMENT0_EXT;
+}
+
+
+
+void GLFBO::attachRenderBuffer( RenderBufferType _type )
+{
+	if ( m_renderBufferIDs[_type] == 0 )
+	{
+		glGenRenderbuffersEXT(1, &m_renderBufferIDs[_type] );
+		glBindRenderbufferEXT(GL_RENDERBUFFER_EXT, m_renderBufferIDs[_type] );
+		glRenderbufferStorageEXT(GL_RENDERBUFFER_EXT, getComponentFromType(_type), m_width, m_height);
+		glFramebufferRenderbufferEXT(GL_FRAMEBUFFER_EXT, getAttachmentFromType(_type), GL_RENDERBUFFER_EXT, m_renderBufferIDs[_type]);
+	}
+	else
+	{
+		assert(0);
+	}
+
+}
---
+++ src/sdsslib/glFBO.h
@@ -0,0 +1,79 @@
+//! \verbatim
+//! ###########################################################################
+//! # SDSS sorting prototype - Stage I
+//! #
+//! # Copyright (c) 2009 Aick in der Au
+//! # All rights reserved.
+//! ###########################################################################
+//!
+//!      created by : Aick in der Au <aick.inderau@gmail.com>
+//!      created on : 7/4/2009
+//! additional docs : none
+//!  responsibility : 1. Aick in der Au
+//!                   2. 
+//! \endverbatim
+//!
+//! \file  glFBO.h
+//! \brief Plainstupid FBO implementation.
+
+#ifndef H_GLFBO
+#define H_GLFBO
+
+#include <Windows.h>
+#include <gl/gl.h>
+
+
+// simple FBO class.
+// usage:
+//
+// GLFBO fbo(640,480);
+// fbo.attachRenderBuffer(GLFBO::RB_DEPTH);
+// fbo.attachRenderBuffer(GLFBO::RB_COLOR0);
+// if (fbo.isComplete())
+// {
+//     fbo.bind();
+//	   doRender();
+//    fbo.unbind();
+// }
+class GLFBO
+{
+public:
+	GLFBO( unsigned int _widht, unsigned int _height );
+	~GLFBO();
+
+	enum RenderBufferType
+	{
+		RB_COLOR0=0,
+		RB_COLOR1=1,
+		RB_DEPTH=2,
+		RB_STENCIL=3,
+		RB_COUNT=4
+	};
+
+	unsigned int getID() const;
+
+	bool isComplete() const;
+
+	void bind();
+	void unbind();
+
+	void attachRenderBuffer( RenderBufferType _type );
+
+private:
+
+	void destroy();
+	
+	GLenum getComponentFromType( RenderBufferType _type );
+	GLenum getAttachmentFromType( RenderBufferType _type );
+
+	unsigned int m_id;
+	unsigned int m_oldId;
+	unsigned int m_width;
+	unsigned int m_height;
+	unsigned int m_renderBufferIDs[RB_COUNT];
+	bool m_bIsBound;
+
+};
+
+#endif 
+
