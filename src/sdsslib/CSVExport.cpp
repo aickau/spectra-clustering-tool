c2a48ed3689d28f2ac2cea9414aa3549c15999c1
Aick <Aick@968c2a35-807f-3149-efd7-b5b61160118a>
2009-11-15 19:00:53 +0000

    -added simple CSV exporter -adapted Spectra::saveToCSV()

---
+++ src/sdsslib/CSVExport.cpp
@@ -0,0 +1,69 @@
+//! \verbatim
+//! ###########################################################################
+//! # SDSS sorting prototype - Stage I
+//! #
+//! # Copyright (c) 2009 Aick in der Au
+//! # All rights reserved.
+//! ###########################################################################
+//!
+//!      created by : Aick in der Au <aick.inderau@gmail.com>
+//!      created on : 11/15/2009
+//! additional docs : none
+//!  responsibility : 1. Aick in der Au
+//!                   2. 
+//! \endverbatim
+//!
+//! \file  CSVExport.cpp
+#include "sdsslib/CSVExport.h"
+
+#include "sdsslib/helpers.h"
+
+#include <sstream>
+#include <fstream>
+
+
+CSVExport::CSVExport(const std::string &_sstrFileName, const std::string &_sstrSeperator )
+:m_sstrFilename(_sstrFileName)
+,m_sstrSeperator(_sstrSeperator)
+,m_sstrTable("")
+{
+}
+
+CSVExport::~CSVExport()
+{
+	flush();
+}
+
+void CSVExport::writeTableEntry(std::string &_sstrText )
+{
+	// todo, add double-quote characters if necessary
+	m_sstrTable += _sstrText;
+	m_sstrTable += m_sstrSeperator;
+}
+
+
+void CSVExport::writeTableEntry( float _number )
+{
+	m_sstrTable += Helpers::numberToString(_number);
+	m_sstrTable += m_sstrSeperator;
+}
+
+
+void CSVExport::writeTableEntry( int _number, unsigned int _width )
+{
+	m_sstrTable += Helpers::numberToString(_number,_width);
+	m_sstrTable += m_sstrSeperator;
+}
+
+void CSVExport::newRow()
+{
+	m_sstrTable += "\n";
+}
+
+
+void CSVExport::flush()
+{
+	std::ofstream fon(std::string(m_sstrFilename).c_str());
+	fon<<m_sstrTable;
+}
+
---
+++ src/sdsslib/CSVExport.h
@@ -0,0 +1,52 @@
+//! \verbatim
+//! ###########################################################################
+//! # SDSS sorting prototype - Stage I
+//! #
+//! # Copyright (c) 2009 Aick in der Au
+//! # All rights reserved.
+//! ###########################################################################
+//!
+//!      created by : Aick in der Au <aick.inderau@gmail.com>
+//!      created on : 11/15/2009
+//! additional docs : none
+//!  responsibility : 1. Aick in der Au
+//!                   2. 
+//! \endverbatim
+//!
+//! \file  CSVExport.h
+//! \brief Simpler saver class to write CSV table entries.
+
+
+// Simpler saver class to write CSV table entries.
+// See http://en.wikipedia.org/wiki/Comma-separated_values
+#ifndef __CSVEXPORT_H__
+#define __CSVEXPORT_H__
+
+#include <string>
+
+
+class CSVExport
+{
+public:
+	CSVExport(const std::string &_sstrFileName, const std::string &_sstrSeperator=";" );
+	virtual ~CSVExport();
+
+	void writeTableEntry(std::string &_sstrText );
+	void writeTableEntry( float _number );
+
+	// \param _width fill number with preceding zeros
+	void writeTableEntry( int _number, unsigned int _width = -1 );
+
+	void newRow();
+
+	// flush table to disk.
+	void flush();
+
+private:
+	const std::string m_sstrSeperator;
+	const std::string m_sstrFilename;
+	std::string m_sstrTable;
+};
+
+
+#endif // __CSVEXPORT_H__
