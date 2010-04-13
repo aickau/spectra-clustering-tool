949175eadae106a4fb5291ab060711f90e2972f1
Aick <Aick@968c2a35-807f-3149-efd7-b5b61160118a>
2010-04-13 21:18:11 +0000

    -added export of various maps

--- trunk/src/specObjOperations/specObjOperations.cpp
+++ trunk/src/specObjOperations/specObjOperations.cpp
@@ -14,7 +14,7 @@
 //! \endverbatim
 //!
 //! \file  specObjOperations.cpp
-//! \brief program to generate binary dumps from a directory of FITS files.
+//! \brief all kinds of operational stuff that has to be quick hacked
 
 
 #include <conio.h>
@@ -33,13 +33,15 @@
 #include "sdsslib/helpers.h"
 #include "sdsslib/filehelpers.h"
 #include "sdsslib/spectraWrite.h"
+#include "sdsslib/spectraVFS.h"
+#include "sdsslib/spectraHelpers.h"
+#include "sdsslib/spectra.h"
 
 typedef char _TCHAR;
 
 //#define DATADIR std::string("G:/SDSS_ANALYZE/fits/spectro/data/*")
 //#define DATADIR std::string("c:/prj/sdss_trunk/data/0266/*")
 #define DATADIR std::string("d:/dr7/1d_25/*")
-
 
 
 
@@ -51,34 +53,183 @@ int fiberID;
 
 };
 
-
-
-
-std::string getSpecObjFileName(TspecObj &obj)
+void trackCatalogs()
 {
-	// e.g. spSpec-52203-0716-002.fit
-	std::string sstrFileName( "spSpec-" );
+	const size_t gridSize(859);
+	const size_t gridSizeSqr(gridSize*gridSize);
 
-	char buf[64];
-	sprintf_s( buf, "%05i", obj.mjd );
-	sstrFileName += buf;
-	sstrFileName += "-";
+	SpectraVFS *pSourceVFS = new SpectraVFS( "allSpectra.bin", false );
+	const size_t numSourceSpecra = pSourceVFS->getNumSpectra();
 
-	sprintf_s( buf, "%04i", obj.plate );
-	sstrFileName += buf;
-	sstrFileName += "-";
+	for (size_t j=0;j<=200;j++)
+	{
+		std::string sstrCatalogueName("catalogueKoester2006");
+		//std::string sstrCatalogueName("catalogueHall2002");
+		const std::string sstrCatalogueName_(sstrCatalogueName+"_"+Helpers::numberToString(j,3));
+		sstrCatalogueName += ".csv";
 
-	sprintf_s( buf, "%03i", obj.fiberID );
-	sstrFileName += buf;
-	sstrFileName += ".fit";
+		std::ofstream logFile(std::string(sstrCatalogueName_+"_log.txt").c_str());
+		std::ifstream fin(sstrCatalogueName.c_str());
 
-	return sstrFileName;
+
+		int *pIndexlist= new int[gridSize*gridSize];
+		std::string sstrIndexList = "indexlist";
+		sstrIndexList += Helpers::numberToString(j,4);
+		sstrIndexList+= ".bin";
+		FILE *f=fopen(sstrIndexList.c_str(),"rb");
+		if ( f!= NULL)
+		{
+			fread(pIndexlist, 1, gridSizeSqr*sizeof(int), f);
+			fclose(f);
+		}
+
+		std::string sstrExport("");
+		sstrExport += "specFilename y x\n";
+		float *pRGBMap = new float[gridSizeSqr*3];
+		for ( size_t i=0;i<gridSizeSqr*3;i++)
+		{
+			pRGBMap[i] = 0.0f;
+		}
+
+
+		while (fin)
+		{
+			std::string sstrPlate, sstrMJD, sstrFiberID;
+			fin >> sstrMJD;
+			fin >> sstrPlate;
+			fin >> sstrFiberID;
+
+			const int plate = Helpers::stringToNumber<int>(sstrPlate);
+			const int mjd = Helpers::stringToNumber<int>(sstrMJD);
+			const int fiberID = Helpers::stringToNumber<int>(sstrFiberID);
+			std::string sstrFilename( Spectra::getSpecObjFileName(plate,mjd,fiberID));
+
+			for (size_t i=0;i<gridSizeSqr;i++)
+			{
+				int index = pIndexlist[i];
+				if ( index > 0 && index < numSourceSpecra )
+				{
+					Spectra *sp = pSourceVFS->beginRead(index);
+					int spFiber = sp->getFiber();
+					int spPlate = sp->getPlate();
+					int spMJD = sp->getMJD();
+					if ( spFiber == fiberID && 
+						spPlate == plate &&
+						spMJD == mjd )
+					{
+						sstrExport += sstrFilename;
+						sstrExport += " ";
+						sstrExport += Helpers::numberToString(i/gridSize);
+						sstrExport += " ";
+						sstrExport += Helpers::numberToString(i%gridSize);
+						sstrExport += "\n";
+
+						pRGBMap[i*3] = 1.0f;
+						pRGBMap[i*3+1] = (float)fiberID/640.f;
+						pRGBMap[i*3+2] = (float)plate/1700.0;
+						i=gridSizeSqr;
+					}
+					pSourceVFS->endRead(index);
+				}
+			}
+		}
+		logFile << sstrExport;
+
+		SpectraHelpers::saveIntensityMap( pRGBMap, gridSize, gridSize, sstrCatalogueName_);
+
+
+		delete[] pIndexlist;
+		delete[] pRGBMap;
+	}
 }
 
-void main(int argc, char* argv[])
+
+void writeSpectrTypes()
+{
+	const size_t gridSize(859);
+	const size_t gridSizeSqr(gridSize*gridSize);
+
+	SpectraVFS *pSourceVFS = new SpectraVFS( "allSpectra.bin", false );
+	const size_t numSourceSpecra = pSourceVFS->getNumSpectra();
+
+	for (size_t j=0;j<=200;j++)
+	{
+		int *pIndexlist= new int[gridSize*gridSize];
+		std::string sstrIndexList = "indexlist";
+		std::string sstrFileName = "objectTypeMap";
+		sstrIndexList += Helpers::numberToString(j,4);
+		sstrFileName += Helpers::numberToString(j,4);
+		sstrIndexList+= ".bin";
+		FILE *f=fopen(sstrIndexList.c_str(),"rb");
+		if ( f!= NULL)
+		{
+			fread(pIndexlist, 1, gridSizeSqr*sizeof(int), f);
+			fclose(f);
+		}
+
+		float *pRGBMap = new float[gridSizeSqr*3];
+		for ( size_t i=0;i<gridSizeSqr*3;i++)
+		{
+			pRGBMap[i] = 0.0f;
+		}
+
+
+		for (size_t i=0;i<gridSizeSqr;i++)
+		{
+			int index = pIndexlist[i];
+			if ( index > 0 && index < numSourceSpecra )
+			{
+				Spectra *sp = pSourceVFS->beginRead(index);
+
+				float r=0;
+				float g=0;
+				float b=0;
+
+				switch ( sp->m_Type )
+				{
+					case Spectra::SPT_NOT_SET : r=0;g=0; b= 0.5; // dark blue
+						break;
+					case Spectra::SPT_SPEC_UNKNOWN : r= 1.0; g=1.0; b= 1.0; // white
+						break;
+					case Spectra::SPT_SPEC_STAR : r= 0.0; g=0.0; b= 1.0; // blue
+						break;
+					case Spectra::SPT_SPEC_GALAXY : r= 1.0; g=0.0; b= 0.0; // dark blue
+						break;
+					case Spectra::SPT_SPEC_QSO : r= 1.0; g=0.5; b= 0.0; // orange
+						break;
+					case Spectra::SPT_SPEC_HIZ_QSO : r= 1.0; g=1.0; b= 0.0; // yellow
+						break;
+					case Spectra::SPT_SPEC_SKY : r= 0.0; g=1.0; b= 1.0;// cyan
+						break;
+					case Spectra::SPT_STAR_LATE : r= 0.0; g=1.0; b= 0.0;// green
+						break;
+					case Spectra::SPT_GAL_EM : r= 1.0; g=0.0; b= 1.0;// violet
+						break;
+				}
+
+				pRGBMap[i*3] =r;
+				pRGBMap[i*3+1] = g;
+				pRGBMap[i*3+2] = b;
+				pSourceVFS->endRead(index);
+			}
+		}
+
+		SpectraHelpers::saveIntensityMap( pRGBMap, gridSize, gridSize, sstrFileName);
+
+
+		delete[] pIndexlist;
+		delete[] pRGBMap;
+	}
+}
+
+
+void spectroLisWrite()
 {
 	std::ofstream logFile("specObjOperations_log.txt");
+	typedef std::map<std::string,TspecObj> TSpecObjMap;
+	TSpecObjMap filenameList;
 
+	std::string plate,mjd,fiberID;
 
 	std::ifstream fin("all_specObjDr4.csv");
 
@@ -88,13 +239,6 @@ void main(int argc, char* argv[])
 	fin >> line;
 	fin >> line;
 
-
-	typedef std::map<std::string,TspecObj> TSpecObjMap;
-	TSpecObjMap filenameList;
-
-
-
-	std::string plate,mjd,fiberID;
 
 
 	size_t fcount=0;
@@ -110,7 +254,7 @@ void main(int argc, char* argv[])
 		specObj.plate = Helpers::stringToNumber<int>(plate);
 		specObj.mjd = Helpers::stringToNumber<int>(mjd);
 
-		std::string sstrFileName = getSpecObjFileName(specObj);
+		std::string sstrFileName = Spectra::getSpecObjFileName(specObj.plate, specObj.mjd, specObj.fiberID );
 		std::pair<TSpecObjMap::iterator, bool> retVal =  filenameList.insert( std::make_pair<std::string,TspecObj>(sstrFileName, specObj));
 
 		if ( !retVal.second )
@@ -165,8 +309,15 @@ void main(int argc, char* argv[])
 	}
 
 	Helpers::print( Helpers::numberToString<int>(numFilesMissing) + std::string(" missing.\n"), &logFile );
+}
 
-		
+void main(int argc, char* argv[])
+{
+	SpectraHelpers::init(0);
+
+	//spectroLisWrite();
+	//trackCatalogs();
+	writeSpectrTypes();
 
 	printf ("fin.\n" );
 
