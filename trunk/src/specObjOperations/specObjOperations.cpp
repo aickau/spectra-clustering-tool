042d83fdb8658b8a0690a00dd567174bce4f3be0
Aick <Aick@968c2a35-807f-3149-efd7-b5b61160118a>
2010-04-18 21:24:52 +0000

    -added even more export of various maps

--- trunk/src/specObjOperations/specObjOperations.cpp
+++ trunk/src/specObjOperations/specObjOperations.cpp
@@ -30,6 +30,10 @@
 #include <set>
 #include <map>
 
+#include <windows.h>
+#include "cfitsio/fitsio.h"
+#include "cfitsio/longnam.h"
+
 #include "sdsslib/helpers.h"
 #include "sdsslib/filehelpers.h"
 #include "sdsslib/spectraWrite.h"
@@ -223,6 +227,280 @@ void writeSpectrTypes()
 }
 
 
+void writeFlux()
+{
+	const size_t gridSize(859);
+	const size_t gridSizeSqr(gridSize*gridSize);
+
+	SpectraVFS *pSourceVFS = new SpectraVFS( "allSpectra.bin", false );
+	const size_t numSourceSpecra = pSourceVFS->getNumSpectra();
+
+	float maxFlux = 0.0f;
+
+	for ( size_t i=0;i<numSourceSpecra;i++)
+	{
+		Spectra *sp = pSourceVFS->beginRead(i);
+		sp->calculateFlux();
+		if ( sp->m_flux > maxFlux)
+			maxFlux = sp->m_flux;
+		pSourceVFS->endRead(i);
+	}
+
+	//for (size_t j=0;j<=200;j++)
+	size_t j=200;
+	{
+		int *pIndexlist= new int[gridSize*gridSize];
+		std::string sstrIndexList = "indexlist";
+		std::string sstrFileName = "fluxMap";
+		sstrIndexList += Helpers::numberToString(j,4);
+		sstrFileName += Helpers::numberToString(j,4);
+		sstrIndexList+= ".bin";
+		FILE *f=fopen(sstrIndexList.c_str(),"rb");
+		if ( f!= NULL)
+		{
+			fread(pIndexlist, 1, gridSizeSqr*sizeof(int), f);
+			fclose(f);
+		}
+
+		float *pRGBMap = new float[gridSizeSqr*3];
+		for ( size_t i=0;i<gridSizeSqr*3;i++)
+		{
+			pRGBMap[i] = 0.0f;
+		}
+
+
+		for (size_t i=0;i<gridSizeSqr;i++)
+		{
+			int index = pIndexlist[i];
+			if ( index > 0 && index < numSourceSpecra )
+			{
+				Spectra *sp = pSourceVFS->beginRead(index);
+				float intensity = log10f(sp->m_flux)/log10f(maxFlux);
+				SpectraHelpers::intensityToRGB( intensity, &pRGBMap[i*3], true );			
+				pSourceVFS->endRead(index);
+			}
+		}
+
+		SpectraHelpers::saveIntensityMap( pRGBMap, gridSize, gridSize, sstrFileName);
+
+
+		delete[] pIndexlist;
+		delete[] pRGBMap;
+	}
+}
+
+
+
+void writePlate()
+{
+	const size_t gridSize(859);
+	const size_t gridSizeSqr(gridSize*gridSize);
+
+	SpectraVFS *pSourceVFS = new SpectraVFS( "allSpectra.bin", false );
+	const size_t numSourceSpecra = pSourceVFS->getNumSpectra();
+
+	int maxPlateID=0;
+	for ( size_t i=0;i<numSourceSpecra;i++)
+	{
+		Spectra *sp = pSourceVFS->beginRead(i);
+		if ( sp->getPlate() > maxPlateID)
+			maxPlateID = sp->getPlate();
+		pSourceVFS->endRead(i);
+	}
+
+
+	//for (size_t j=0;j<=200;j++)
+	size_t j=200;
+	{
+		int *pIndexlist= new int[gridSize*gridSize];
+		std::string sstrIndexList = "indexlist";
+		std::string sstrFileName = "plateIDMap";
+		sstrIndexList += Helpers::numberToString(j,4);
+		sstrFileName += Helpers::numberToString(j,4);
+		sstrIndexList+= ".bin";
+		FILE *f=fopen(sstrIndexList.c_str(),"rb");
+		if ( f!= NULL)
+		{
+			fread(pIndexlist, 1, gridSizeSqr*sizeof(int), f);
+			fclose(f);
+		}
+
+		float *pRGBMap = new float[gridSizeSqr*3];
+		for ( size_t i=0;i<gridSizeSqr*3;i++)
+		{
+			pRGBMap[i] = 0.5f;
+		}
+
+
+		for (size_t i=0;i<gridSizeSqr;i++)
+		{
+			int index = pIndexlist[i];
+			if ( index > 0 && index < numSourceSpecra )
+			{
+				Spectra *sp = pSourceVFS->beginRead(index);
+				float intensity = (float)(sp->getPlate()-266)/(float)(maxPlateID-266);
+				SpectraHelpers::intensityToRGB( intensity, &pRGBMap[i*3], false );			
+				pSourceVFS->endRead(index);
+			}
+		}
+
+		SpectraHelpers::saveIntensityMap( pRGBMap, gridSize, gridSize, sstrFileName);
+
+
+		delete[] pIndexlist;
+		delete[] pRGBMap;
+	}
+}
+
+
+
+
+void writeMJD()
+{
+	const size_t gridSize(859);
+	const size_t gridSizeSqr(gridSize*gridSize);
+
+	SpectraVFS *pSourceVFS = new SpectraVFS( "allSpectra.bin", false );
+	const size_t numSourceSpecra = pSourceVFS->getNumSpectra();
+
+	int maxMJD=0;
+	int minMJD=0x0ffffff;
+	for ( size_t i=0;i<numSourceSpecra;i++)
+	{
+		Spectra *sp = pSourceVFS->beginRead(i);
+		if ( sp->getMJD() > maxMJD)
+			maxMJD = sp->getMJD();
+		if ( sp->getMJD() < minMJD)
+			minMJD = sp->getMJD();
+		pSourceVFS->endRead(i);
+	}
+
+
+	//for (size_t j=0;j<=200;j++)
+	size_t j=200;
+	{
+		int *pIndexlist= new int[gridSize*gridSize];
+		std::string sstrIndexList = "indexlist";
+		std::string sstrFileName = "MJDMap";
+		sstrIndexList += Helpers::numberToString(j,4);
+		sstrFileName += Helpers::numberToString(j,4);
+		sstrIndexList+= ".bin";
+		FILE *f=fopen(sstrIndexList.c_str(),"rb");
+		if ( f!= NULL)
+		{
+			fread(pIndexlist, 1, gridSizeSqr*sizeof(int), f);
+			fclose(f);
+		}
+
+		float *pRGBMap = new float[gridSizeSqr*3];
+		for ( size_t i=0;i<gridSizeSqr*3;i++)
+		{
+			pRGBMap[i] = 0.5f;
+		}
+
+
+		for (size_t i=0;i<gridSizeSqr;i++)
+		{
+			int index = pIndexlist[i];
+			if ( index > 0 && index < numSourceSpecra )
+			{
+				Spectra *sp = pSourceVFS->beginRead(index);
+				float intensity = (float)(sp->getMJD()-minMJD)/(float)(maxMJD-minMJD);
+				SpectraHelpers::intensityToRGB( intensity, &pRGBMap[i*3], true );			
+				pSourceVFS->endRead(index);
+			}
+		}
+
+		SpectraHelpers::saveIntensityMap( pRGBMap, gridSize, gridSize, sstrFileName);
+
+
+		delete[] pIndexlist;
+		delete[] pRGBMap;
+	}
+}
+
+
+
+void writeRADEC()
+{
+	const size_t gridSize(859);
+	const size_t gridSizeSqr(gridSize*gridSize);
+
+	SpectraVFS *pSourceVFS = new SpectraVFS( "allSpectra.bin", false );
+	const size_t numSourceSpecra = pSourceVFS->getNumSpectra();
+
+	float maxFlux = 0.0f;
+
+	//for (size_t j=0;j<=200;j++)
+	size_t j=200;
+	{
+		int *pIndexlist= new int[gridSize*gridSize];
+		std::string sstrIndexList = "indexlist";
+		std::string sstrFileName = "RaDecMap";
+		sstrIndexList += Helpers::numberToString(j,4);
+		sstrFileName += Helpers::numberToString(j,4);
+		sstrIndexList+= ".bin";
+		FILE *f=fopen(sstrIndexList.c_str(),"rb");
+		if ( f!= NULL)
+		{
+			fread(pIndexlist, 1, gridSizeSqr*sizeof(int), f);
+			fclose(f);
+		}
+
+		float *pRGBMap = new float[gridSizeSqr*3];
+		for ( size_t i=0;i<gridSizeSqr*3;i++)
+		{
+			pRGBMap[i] = 0.0f;
+		}
+
+
+		for (size_t i=0;i<gridSizeSqr;i++)
+		{
+			int index = pIndexlist[i];
+			if ( index > 0 && index < numSourceSpecra )
+			{
+				Spectra *sp = pSourceVFS->beginRead(index);
+				std::string sstrFileName = Spectra::getSpecObjFileName(sp->getPlate(),sp->getMJD(),sp->getFiber());
+				std::string sstrPlate = Helpers::numberToString(sp->getPlate(),4);
+				std::string sstrPath = "D:/dr7/1d_25/"+sstrPlate+"/1d/"+sstrFileName;
+
+
+				fitsfile *f;
+				int status = 0;
+				float ra = 0.f;
+				float dec = 0.0f;
+
+				fits_open_file( &f, sstrPath.c_str(), READONLY, &status );
+				if ( status == 0 )
+				{
+					fits_read_key( f, TFLOAT, "RADEG", &ra, NULL, &status );
+					fits_read_key( f, TFLOAT, "DECDEG", &dec, NULL, &status );
+					fits_close_file(f, &status);
+				}
+				
+				
+				// dec -30..90
+				float r = (dec + 30.f)/120.f;
+				float g = ra/360.f;
+				float b = 0.0f;
+				
+				pRGBMap[i*3] = r;
+				pRGBMap[i*3+1] = g;
+				pRGBMap[i*3+2] = b;
+				
+				pSourceVFS->endRead(index);
+			}
+		}
+
+		SpectraHelpers::saveIntensityMap( pRGBMap, gridSize, gridSize, sstrFileName);
+
+
+		delete[] pIndexlist;
+		delete[] pRGBMap;
+	}
+}
+
+
 void spectroLisWrite()
 {
 	std::ofstream logFile("specObjOperations_log.txt");
@@ -282,7 +560,7 @@ void spectroLisWrite()
 		if ( !retVal.second )
 		{
 			Helpers::print( sstrFilename+std::string(" inserted twice!\n"), &logFile );
-		}
+		}	
 	}
 	Helpers::print( Helpers::numberToString<int>(numFiles) + std::string(" in directory ")+DATADIR+std::string(".\n"), &logFile );
 
@@ -317,7 +595,11 @@ void main(int argc, char* argv[])
 
 	//spectroLisWrite();
 	//trackCatalogs();
-	writeSpectrTypes();
+	//writeSpectrTypes();
+	//writeFlux();
+	//writePlate();
+	//writeRADEC();
+	writeMJD();
 
 	printf ("fin.\n" );
 
