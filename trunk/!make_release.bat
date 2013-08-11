dc91ddfdd46356d625b95b2c8536acad1d697b5c
aau <aau@968c2a35-807f-3149-efd7-b5b61160118a>
2013-08-11 15:35:54 +0000

    new release script

---
+++ trunk/!make_release.bat
@@ -0,0 +1,30 @@
+
+set ReleaseDir=c:/incoming/sdss
+
+mkdir "%ReleaseDir%/export"
+
+for %%d in (analyzer dump search compare spectraMapper) do (
+	copy "%%dWin32Release.exe" "%ReleaseDir%" /Y /B 
+	copy "%%dx64Release.exe" "%ReleaseDir%" /Y /B 
+)
+
+for %%d in (IL ILU) do (
+	copy "%%dWin32.dll" "%ReleaseDir%" /Y /B 
+	copy "%%dx64.dll" "%ReleaseDir%" /Y /B 
+)
+
+
+copy "settings.xml" "%ReleaseDir%" /Y /B
+copy "spectraParamDR9.bin" "%ReleaseDir%" /Y /B
+copy "spectraParamDR10.bin" "%ReleaseDir%" /Y /B
+
+cd export
+for %%d in (up down left right empty) do (
+	copy "%%d.png" "%ReleaseDir%/export" /Y /B 
+)
+
+for %%d in (!DELETE_ALL !DELETE_HTMLS !DELETE_ICONS) do (
+	copy "%%d.bat" "%ReleaseDir%/export" /Y /B 
+)
+
+cd..
--- trunk/export/!DELETE_ALL.bat
+++ trunk/export/!DELETE_ALL.bat
@@ -1,6 +1,8 @@
 del /S *.PNG
 del /S *.html
 del /S *.txt
+del /S *.bin
+del /S *.csv
 
 
 
