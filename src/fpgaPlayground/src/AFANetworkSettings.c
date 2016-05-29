e59c11a1c56542959a60353f095f3cabc48963dd
aickau <aickau@users.noreply.github.com>
2016-05-29 08:53:46 +0200

    -synced settings file afa / vanilla

--- src/fpgaPlayground/src/AFANetworkSettings.c
+++ src/fpgaPlayground/src/AFANetworkSettings.c
@@ -10,14 +10,14 @@ AFASetDefaultParameters(
     AFAParameters *params )
 {
     params->numSteps = 200;
-    params->randomSeed = 15;
+    params->randomSeed = 23;
     params->lRateBegin = 0.25f;
     params->lRateEnd = 0.01f;
     params->radiusBegin = 1.0f;
     params->radiusEnd = 0.125f;
     params->searchMode = AFANET_SETTINGS_SEARCHMODE_global;
     params->normaliziationType = SN_FLUX;
-    params->useBOSSWavelengthRange = TRUE;
+    params->useBOSSWavelengthRange = FALSE;
 }
 #endif
 
