2453dac0ca1d711135656355dbfd359ba83254f5
Blörg <devel@bloerg.de>
2016-08-09 10:58:31 +0200

    software Version placeholder for easy quick and dirty compilaton

---
+++ src/sdsslib/sdssSoftwareVersion.h
@@ -0,0 +1,23 @@
+//! \verbatim
+//! ###########################################################################
+//! # ASPECT: A spectra clustering tool for exploration of large spectral surveys - Stage I
+//! #
+//! # Copyright (c) 2009 Aick in der Au
+//! # All rights reserved.
+//! ###########################################################################
+//!
+//!      created by : Aick in der Au <aick.inderau@gmail.com>
+//!      created on : 5/9/2009
+//! additional docs : none
+//!  responsibility : 1. Aick in der Au
+//!                   2. 
+//! \endverbatim
+//!
+//! \file  sdssSoftwareVersionTemplate.h
+//! \brief holds the version string
+
+const std::string sstrVersion("linux");
+const std::string sstrSubversionRev("Subersionplaceholder");
+const std::string sstrBuildDate("Builddateplaceholder");
+const std::string sstrSDSSVersionString("Version "+sstrVersion+", Revision "+sstrSubversionRev+", Build date "+sstrBuildDate);
+
